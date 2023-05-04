d0f5c112e91f14757db16d775adea15f3edab698
Vincent <vincentabensour@yahoo.fr>
2023-05-04 09:34:38 +0200

    Update Head with meta description

--- components/Head.js
+++ components/Head.js
@@ -5,6 +5,11 @@ function IndexPage(props) {
       <Head>
         <title>{props.title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Mon site personnel où vous pourrez retrouver mes projets, ma bio et me contacter!"
+          key="desc"
+        />
         <link rel="icon" type="image/png" src="/logo/logo.png" />
       </Head>
   )
