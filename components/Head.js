c594598c8c6502bf2352538efa3dc0d542cefe26
Vincent <vincentabensour@yahoo.fr>
2023-06-19 18:18:49 +0200

    Update Head.js

--- components/Head.js
+++ components/Head.js
@@ -7,7 +7,7 @@ function IndexPage(props) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
-          content="Mon site personnel où vous pourrez retrouver mes projets, ma bio et me contacter!"
+          content="Voici mon site personnel de développeur web où vous pourrez retrouver mes projets, ma biographie. N'hésitez pas à me contacter pour votre projet web!"
           keyword="desc"
         />
         <link rel="icon" type="image/png" href="/logo/logo.png" />
