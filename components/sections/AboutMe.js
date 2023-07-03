9e851842b79f1b71ddd15054d487f92709bce334
Vincent <vincentabensour@yahoo.fr>
2023-07-03 19:46:31 +0200

    Update AboutMe.js

--- components/sections/AboutMe.js
+++ components/sections/AboutMe.js
@@ -9,12 +9,12 @@ export default function AboutMe() {
         </motion.h2>
         <div>
             <motion.p className='mb-4 lg:mb-8' initial={{opacity:0, x:20}} whileInView={{opacity: 1, x:0}} transition={{duration:0.5, delay:0.2}} viewport={{ once: true }}>Hello! je m&apos;appelle <span className="text-highlight">Vincent</span> et je suis un développeur web full-stack auto-didacte. J&apos;ai commencé ma carrière dans le développement de projet, plus particulièrement pour des associations et des entreprises à impact social et environnemental.
-                J'ai vu ma première ligne de code en programmant des Arduino pour un projet de sensibilisation à la qualité de l&apos;air.
+                J&apos;ai vu ma première ligne de code en programmant des Arduino pour un projet de sensibilisation à la qualité de l&apos;air.
                 A partir de là mon intérêt pour le développement web n&apos;a cessé de grandir et je me suis logiquement mis à apprendre le HTML, CSS et le JavaScript!
             </motion.p>
 
             <motion.p className='mb-4 lg:mb-8' initial={{opacity:0, x:-20}} whileInView={{opacity: 1, x:0}} transition={{duration:0.5, delay:0.2}} viewport={{ once: true }}>Comme la vie est faite de choix, j&apos;ai décidé en 2022 d&apos;en faire un particulèrement important puisque je me suis mis à coder à plein temps.
-                J'ai commencé par Python &#40;probablement par ce que j&apos;aimais bien le nom&#41; et c&apos;était une super aventure. Après quelques petits projets j&apos;ai eu ma première idée de site internet, un site de vente en ligne pour les mairachers avec un dashboard d&apos;aide à la récolte basé sur les paniers commandés par les clients.
+                J&apos;ai commencé par Python &#40;probablement par ce que j&apos;aimais bien le nom&#41; et c&apos;était une super aventure. Après quelques petits projets j&apos;ai eu ma première idée de site internet, un site de vente en ligne pour les mairachers avec un dashboard d&apos;aide à la récolte basé sur les paniers commandés par les clients.
             </motion.p>
 
             <motion.p className='mb-4 lg:mb-8' initial={{opacity:0, x:20}} whileInView={{opacity: 1, x:0}} transition={{duration:0.5, delay:0.2}} viewport={{ once: true }}>
